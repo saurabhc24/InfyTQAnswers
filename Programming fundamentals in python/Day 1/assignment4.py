482a45388ffcc2a24888371b2fd83a2b3760ac4b
Saurabh Chandra <49842985+saurabhc24@users.noreply.github.com>
2020-08-15 00:51:13 +0530

    Create assignment4.py

---
+++ Programming fundamentals in python/Day 1/assignment4.py
@@ -0,0 +1,16 @@
+# Question : Write a Python program to calculate and display the interest on a loan amount (Rupees) using the formula:
+# interest=(principal * rate of interest * time)/100
+# use principal = 7800, rate = 7.7 and time = 26 to verify the code
+
+principal=7800
+rate_of_interest=7.7
+time=26
+interest=0
+
+#Start writing your code from here
+#Populate the variable: interest
+interest=(principal * rate_of_interest * time)/100
+
+
+#Do not modify the below print statement for verification to work
+print(interest)
