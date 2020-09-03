3f90ea8194e24aa372d0a2044660aee25e2e4d6a
Saurabh Chandra <49842985+saurabhc24@users.noreply.github.com>
2020-09-03 21:30:58 +0530

    Added Day 2 Exercise 12 sol

--- Programming fundamentals in python/Day 2/Exercise/Exercise_12.go
+++ Programming fundamentals in python/Day 2/Exercise/Exercise_12.go
@@ -16,3 +16,23 @@ Verify your code by using the 2nd sample input(highlighted) given below:
 +--------------------+-----------------+
 */
 
+//PF-Exer-12
+//This verification is based on string match.
+
+package main
+import ("fmt")
+func main(){
+    //Write your program logic here
+    var num1 int = 3
+    var num2 int = 4 
+    var num3 int = 1
+    if(num1>num2 && num1>num3){
+        fmt.Println(num1)
+    }
+    if (num2>=num1 && num2>=num3){
+        fmt.Println(num2)
+    }
+    if (num3>=num1 && num3>=num2) {
+        fmt.Println(num3)
+    }
+}
