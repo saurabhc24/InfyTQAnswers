9e12d6cd2c9d7c06520ec041b135301dd8858e1c
Saurabh Chandra <49842985+saurabhc24@users.noreply.github.com>
2020-09-03 21:25:23 +0530

    Create Exercise_12.go

---
+++ Programming fundamentals in python/Day 2/Exercise/Exercise_12.go
@@ -0,0 +1,18 @@
+/*
+Write a Go program to find and display the maximum of three given numbers.
+
+
+Test your code by using the given sample inputs.
+Verify your code by using the 2nd sample input(highlighted) given below:
+
++--------------------+-----------------+
+|     Sample Input   | Expected Output |
++------+------+------+-----------------+
+| Num1 | Num2 | Num3 |	               |
++------+------+------+-----------------+
+|   1  |   2  |   3  |       3         |
++------+------+------+-----------------+
+|   3  |   4  |   1  |	  Test Case    |
++--------------------+-----------------+
+*/
+
