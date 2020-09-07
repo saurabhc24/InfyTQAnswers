f9541b978c6ed3db641bc7e24d8a75d694efd4a9
Saurabh Chandra <49842985+saurabhc24@users.noreply.github.com>
2020-09-08 02:08:18 +0530

    Create Assignment_18.py

---
+++ Programming fundamentals in python/Day 2/Assignment_18.py
@@ -0,0 +1,22 @@
+"""
+A traveler on a visit to India is in need of some Indian Rupees (INR) but he has money belonging to another currency. 
+He wants to know how much money he should provide in the currency he has, to get the specified amount in INR.
+
+Write a python program to implement a currency calculator which accepts the amount needed in INR and the name of the currency which the traveler has. 
+The program should identify and display the amount the traveler should provide in the currency he has, to get the specified amount in INR.
+
+Note: Use the forex information provided in the table below for the calculation. Consider that only the currency names mentioned in the table are valid. For any invalid currency name, display -1.
+
++-------------------+-------------------------+
+|      Currency     |  Equivalent of 1.00 INR |
++-------------------+-------------------------+
+|        Euro       |          0.01417        |
++-------------------+-------------------------+
+|   British Pound   |          0.0100         |
++-------------------+-------------------------+
+| Australian Dollar |          0.02140        |
++-------------------+-------------------------+
+|  Canadian Dollar  |          0.02027        |
++-------------------+-------------------------+
+"""
+
